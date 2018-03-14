cddd336edd5cf19b8d0509414e969f8938772e90
JeonghyunYoo <yoojh9@hancomm.com>
2018-03-14 11:17:51 +0900

    ch14.비동기적 프로그래밍

---
+++ ch14/callback-this-test.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5];
+
+let obj = {
+  vals: [1,2,3],
+  logValues: function(v, i) {
+    if(this.vals){
+      console.log(this.vals, v, i);
+    } else {
+      console.log(this, v, i);
+    }
+  }
+};
+
+obj.logValues(1,2); // 이 때 this는 obj
+//arr.forEach(obj.logValues);  // arr.forEach의 콜백함수로써 obj.logValues를 호출함. 
+                             //이 때 this는 arr.forEach의 this와 같으며
+                             // arr.forEach의 this는 따로 설정되어 있지 않으므로 this는 window 객체와 같다 
