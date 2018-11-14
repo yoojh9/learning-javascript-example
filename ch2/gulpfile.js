8f29f6f573bda007d5062f94e43ff3a028aa573a
JeonghyunYoo <yoojh9@hancomm.com>
2018-11-14 15:43:25 +0900

    코드 변경

--- ch2/gulpfile.js
+++ ch2/gulpfile.js
@@ -7,10 +7,11 @@ gulp.task('default', function(){
 	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
 		.pipe(eslint())
 		.pipe(eslint.format());
+
 	// 노드 소스
 	gulp.src("es6/**/*.js")
-		.pipe(babel())
-		.pipe(gulp.dest("dist"));
+		.pipe(babel()) 	// 이 소스 파일을 바벨에 파이프로 연결. 바벨은 ES6 -> ES5로 변형함.
+		.pipe(gulp.dest("dist"));	// 컴파일 된 ES5 코드를 dist 디렉토리에 저장.
 	// 브라우저 소스
 	gulp.src("public/es6/**/*.js")
 		.pipe(babel())
